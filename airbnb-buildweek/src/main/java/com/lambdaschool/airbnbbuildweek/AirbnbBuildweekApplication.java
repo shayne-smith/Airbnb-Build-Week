8de1af8dce6b40564b7ba79f4a44abb44742d4b6
seanmx96 <seanmx96@users.noreply.github.com>
2020-06-20 00:38:56 -0500

    Setup project

---
+++ airbnb-buildweek/src/main/java/com/lambdaschool/airbnbbuildweek/AirbnbBuildweekApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.airbnbbuildweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbBuildweekApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AirbnbBuildweekApplication.class,
+            args);
+    }
+
+}
---
+++ airbnb-buildweek/src/test/java/com/lambdaschool/airbnbbuildweek/AirbnbBuildweekApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.airbnbbuildweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbBuildweekApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
